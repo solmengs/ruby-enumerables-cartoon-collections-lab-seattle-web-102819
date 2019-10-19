1a4a5b9f2da4f90e3e7feaa1fc6998e8a2a7ab1b
mengistiab woldearegay <solmengs@gmail.com>
2019-10-19 09:17:39 +0000

    Done.

--- spec/cartoon_collections_spec.rb
+++ spec/cartoon_collections_spec.rb
@@ -1,68 +1,72 @@
-require "spec_helper"
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
+planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+call_screams = ["bo", "bam", "boom", "bazam", "powzer"]
 
-describe "Cartoon Collections" do
-  describe "#roll_call_dwarves" do
-    it "prints out the 7 dwarfs in a numbered list" do
-      dwarves = ["Dopey", "Grumpy", "Bashful"]
-
-      output = capture_stdout do
-        roll_call_dwarves(dwarves)
-      end
-
-      # This regex allows for arbitrary characters between
-      # the numbering and the name
-      expect(output).to match(/1.*Dopey/)
-      expect(output).to match(/2.*Grumpy/)
-      expect(output).to match(/3.*Bashful/)
-    end
-  end
-
-  describe "#summon_captain_planet" do
-    it "returns an array with the same number of elements that it was given" do
-      veggies = ["carrot", "cucumber", "pepper"]
-      result = summon_captain_planet(veggies)
-      expect(result.class).to eq(Array)
-      expect(result.length).to eq(3)
-    end
-
-    it "capitalizes each element and adds an exclamation mark (test 1)" do
-      fruits = ["apple", "banana", "orange"]
-      result = summon_captain_planet(fruits)
-      ['Apple!', 'Banana!', 'Orange!'].each { |w| expect(result).to include w }
-    end
-
-    it "capitalizes each element and adds an exclamation mark (test 2)" do
-      veggies = ["carrot", "cucumber", "pepper"]
-      result = summon_captain_planet(veggies)
-      ["Carrot!", "Cucumber!", "Pepper!"].each { |w| expect(result).to include w }
-    end
-  end
-
-  describe "#long_planeteer_calls" do
-    it "returns true if any calls are longer than 4 characters" do
-      calls_long = ["axe", "earth", "wind", "fire"]
-      expect(long_planeteer_calls(calls_long)).to eq(true)
-    end
-
-    it "returns false if all calls are 4 characters or less" do
-      calls_short = ["wind", "fire", "tree", "axe", "code"]
-      expect(long_planeteer_calls(calls_short)).to eq(false)
-    end
-
-  end
-
-  describe "#find_the_cheese" do
-    it "returns the first element of the array that is cheese" do
-      contains_cheddar = ["banana", "cheddar", "sock"]
-      expect(find_the_cheese(contains_cheddar)).to eq 'cheddar'
-
-      contains_gouda = ["potato", "gouda", "camembert"]
-      expect(find_the_cheese(contains_gouda)).to eq 'gouda'
-    end
-
-    it "returns nil if the array does not contain a type of cheese" do
-      no_cheese = ["ham", "cellphone", "computer"]
-      expect(find_the_cheese(no_cheese)).to eq nil
-    end
-  end
+def roll_call_dwarves(array)
+  i = 0 
+  while i < array.length 
+    puts "#{i + 1}. #{array[i]}"
+    i += 1 
+  end 
 end
+
+roll_call_dwarves(dwarves)
+
+
+
+def summon_captain_planet(array)
+  nu_array = []
+  i = 0 
+  while i < array.length 
+    nu_array << array[i].capitalize + "!"
+    i += 1 
+  end 
+  nu_array
+end
+
+summon_captain_planet(planeteer_calls)
+
+
+def long_planeteer_calls(array) 
+  i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
+end
+
+long_planeteer_calls(call_screams)
+
+
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
+
+# This is the learn.co method that wants the cheese array inside the actual method
+def find_the_cheese(array)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
+find_the_cheese(snacks)
+
+
+
+# This is a much better way cause the cheese array isn't included in the method.
+cheese_types = ["cheddar", "gouda", "camembert"]
+snacks = ["crackers", "gouda", "thyme"]
+
+def find_the_cheese2(array, array2)
+  cheese_types = array2
+    array.find do |type|
+    cheese_types.include?(type)
+  end 
+end
+
+find_the_cheese2(snacks, cheese_types)
+
+
